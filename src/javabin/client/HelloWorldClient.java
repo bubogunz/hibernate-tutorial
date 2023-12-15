a71f1ed77764ff190dc4155036dbd9485ec181f2
bubogunz <federico.brian@gmail.com>
2023-12-15 17:55:11 +0100

    second lesson added

--- src/javabin/client/HelloWorldClient.java
+++ src/javabin/client/HelloWorldClient.java
@@ -1,23 +1,42 @@
 package javabin.client;
 
 import javabin.entity.Message;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
+import org.hibernate.Transaction;
 import util.HibernateUtil;
+
+import java.util.logging.Logger;
 
 public class HelloWorldClient {
     public static void main(String[] args) {
-
+        /*First lesson*/
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
 
-        Message message = new Message( "Yet another message" );
-
+        Message message = new Message("Prova tutorial 2");
         session.persist(message);
 
         session.getTransaction().commit();
-        session.close();
+        /*session.close();*/
 
+        /*Second lesson*/
+        Transaction txn = session.getTransaction();
+        try {
+            txn.begin();
+            Message msg = session.get(Message.class, 2L);
+            msg.setText("Hello Automatic Dirty Checking");
+//            session.delete(msg);
+            txn.commit();
+        } catch (HibernateException e) {
+            if(!java.util.Objects.isNull(txn)) {
+                txn.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        session.close();
     }
 }
 
